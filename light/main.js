e857d460c08ff54a9abb115b965173342a321721
WANG HUA <wanghua@HuaRealibox.local>
2022-12-16 18:47:05 +0800

    update

--- light/main.js
+++ light/main.js
@@ -1,10 +1,12 @@
 import * as THREE from "three";
+import * as dat from "dat.gui";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 import gsap from "gsap";
 import gate from "../images/gate.jpg";
 import "../style.css";
+import checker from "../images/checker.png";
 
 const sizes = {
     width: window.innerWidth,
@@ -14,29 +16,82 @@ const sizes = {
 const loader = new THREE.TextureLoader();
 
 const canvas = document.querySelector("#app");
-const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(2);
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color("black");
 
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100);
-camera.position.set(0,10,20);
-
-
-const geometry = new THREE.SphereGeometry(3, 64, 64);
-
-const mesh = new THREE.Mesh(geometry);
-
-scene.add(mesh);
-
+camera.position.set(0, 10, 20);
 scene.add(camera);
 
-renderer.render(scene, camera);
-
 const controls = new OrbitControls(camera, canvas);
-controls.target.set(0,5,0);
+controls.target.set(0, 5, 0);
 controls.update();
+
+const planeSize = 40;
+
+const texture = loader.load(checker);
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.magFilter = THREE.NearestFilter;
+const repeats = planeSize / 2;
+texture.repeat.set(repeats, repeats);
+
+const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+const planeMat = new THREE.MeshPhongMaterial({
+    map: texture,
+    side: THREE.DoubleSide,
+});
+const mesh = new THREE.Mesh(planeGeo, planeMat);
+mesh.rotation.x = Math.PI * -0.5;
+scene.add(mesh);
+
+{
+    const cubeSize = 4;
+    const cubeGeo = new THREE.BoxBufferGeometry(cubeSize, cubeSize, cubeSize);
+    const cubeMat = new THREE.MeshPhongMaterial({ color: "#8AC" });
+    const mesh = new THREE.Mesh(cubeGeo, cubeMat);
+    mesh.position.set(cubeSize + 1, cubeSize / 2, 0);
+    scene.add(mesh);
+}
+
+{
+    const sphereRadius = 3;
+    const sphereWidthDivisions = 32;
+    const sphereHeightDivisions = 16;
+    const sphereGeo = new THREE.SphereBufferGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions);
+    const sphereMat = new THREE.MeshPhongMaterial({ color: "#CA8" });
+    const mesh = new THREE.Mesh(sphereGeo, sphereMat);
+    mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
+    scene.add(mesh);
+}
+
+class ColorGUIHelper {
+    constructor(object, prop) {
+        this.object = object;
+        this.prop = prop;
+    }
+    get value() {
+        return `#${this.object[this.prop].getHexString()}`;
+    }
+    set value(hexString) {
+        this.object[this.prop].set(hexString);
+    }
+}
+
+const color = 0xffffff;
+const intensity = 1;
+const light = new THREE.AmbientLight(color, intensity);
+scene.add(light);
+
+const gui = new dat.GUI();
+gui.addColor(new ColorGUIHelper(light, "color"), "value").name("color");
+gui.add(light, "intensity", 0, 2, 0.01);
+
+renderer.render(scene, camera);
 
 window.addEventListener("resize", () => {
     sizes.width = window.innerWidth;
@@ -54,23 +109,3 @@ const loop = () => {
 };
 
 loop();
-
-const tl = gsap.timeline({ defaults: { duration: 1 } });
-tl.fromTo(mesh.scale, { x: 0, y: 0, z: 0 }, { x: 1, y: 1, z: 1 });
-
-let mouseDown = false;
-let rgb = [];
-window.addEventListener("mousedown", () => (mouseDown = true));
-window.addEventListener("mouseup", () => (mouseDown = false));
-
-window.addEventListener("mousemove", (e) => {
-    if (mouseDown) {
-        rgb = [
-            Math.round((e.pageX / sizes.width) * 255),
-            Math.round((e.pageY / sizes.height) * 255),
-            Math.round(Math.random() * 255),
-        ];
-        let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
-        gsap.to(mesh.material.color, { r: newColor.r, g: newColor.g, b: newColor.b });
-    }
-});
